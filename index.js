88db9981be632178c3611f813a5516d768cb0c03
pullagoruniraja <niraja@pdx.edu>
2022-08-18 23:09:35 -0700

    dynamic name and email display

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ let resetForm = () => {
   document.getElementById("contact-form").reset();
 };
 
-let form = document.querySelector("#contact-form");
-form.addEventListener("submit", handleSubmit);
-
-function handleSubmit(event) {
+let handleSubmit = () => {
   let name = document.querySelector("#contact-name");
   let email = document.querySelector("#contact-email");
   let displayName = document.querySelector(".display-contact-name");
+  displayName.style.color = "#0F3D3E";
+  displayName.style.fontSize ='1.2em'
   let displayContact = document.querySelector(".display-contact-email");
 
-  displayName.append(name.value + ',');
-  displayContact.append(email.value)
-}
+  displayName.append(name.value + ",");
+  displayContact.append(email.value);
+};
